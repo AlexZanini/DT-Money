dc66a7b5d132b0331f9ff00a37e6fa35b9887664
Alex Zanini <97163043+AlexZanini@users.noreply.github.com>
2023-07-06 10:30:47 -0300

    Add files via upload

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,44 @@
+import { MagnifyingGlass } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { SearchFormContainer } from './styles'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
+import { useContext } from 'react'
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
+export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitted },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="test"
+        placeholder="Busque por Transaçoes"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitted}>
+        <MagnifyingGlass size={20} />
+        Buscar
+      </button>
+    </SearchFormContainer>
+  )
+}
